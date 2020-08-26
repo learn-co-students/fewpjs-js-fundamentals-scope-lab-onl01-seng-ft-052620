a91cd28cb90b998e2eb83e5a1927342082a82aec
SWFullPotential <swfullpotential@gmail.com>
2020-08-26 16:56:54 -0600

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function myAnimal() {
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   animal = "cat"
+  // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
@@ -19,3 +19,26 @@ function add2(n) {
 
   // Feel free to move things around!
 }
+
+/*
+const animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  let animal = 'cat'
+
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+
+  return n + two
+}
+*/
